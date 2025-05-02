d9cbe4f38408fd02c6a7dc7999bba468a6c2a1c5
Aubrey Taylor <aubrey@hall.ly>
2025-05-02 16:55:24 -0600

    change opnix token location on servers

--- system/components/credentials/configuration.nix
+++ system/components/credentials/configuration.nix
@@ -1,7 +1,11 @@
-{...}: {
+{isDesktop, ...}: {
   services.onepassword-secrets = {
     enable = true;
     users = ["rose"];
+    tokenFile =
+      if isDesktop
+      then "/etc/opnix-token"
+      else "/var/opnix-token";
     configFile = ./secrets.json;
   };
 }
