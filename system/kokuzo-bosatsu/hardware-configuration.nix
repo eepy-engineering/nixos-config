ce54825507c7d3b6c4a5995609f08790040cb913
Aubrey Taylor <aubrey@hall.ly>
2025-05-02 23:11:30 -0600

    update boot drive uuid

--- system/kokuzo-bosatsu/hardware-configuration.nix
+++ system/kokuzo-bosatsu/hardware-configuration.nix
@@ -28,7 +28,7 @@
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/12CE-A600";
+    device = "/dev/disk/by-uuid/4FFD-2C3C";
     fsType = "vfat";
     options = ["fmask=0022" "dmask=0022"];
   };
