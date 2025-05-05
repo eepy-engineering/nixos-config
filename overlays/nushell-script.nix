ae1b5a171d420d1c45142991cd24b2a9f6994271
Aubrey Taylor <aubrey@hall.ly>
2025-05-05 15:22:12 -0600

    add non bin variant

--- overlays/nushell-script.nix
+++ overlays/nushell-script.nix
@@ -1,4 +1,16 @@
 final: _prev: {
+  writeNushellScript = name: contents:
+    final.writeTextFile {
+      inherit name;
+      executable = true;
+      checkPhase = ''
+        ${final.nushell}/bin/nu -c "open $target | nu-check -d"
+      '';
+      text = ''
+        #!${final.nushell}/bin/nu
+        ${contents}
+      '';
+    };
   writeNushellScriptBin = name: contents:
     final.writeTextFile {
       inherit name;
