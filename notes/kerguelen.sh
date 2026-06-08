13dc8bce59a53a5502a589c623307e6474415340
Aubrey Taylor <aubrey@hall.ly>
2026-06-08 14:41:00 -0600

    stop adding store option

--- notes/kerguelen.sh
+++ notes/kerguelen.sh
@@ -18,4 +18,4 @@ mount /dev/sda1 /mnt/boot
 mount -o compress=zstd,subvol=persist /dev/sda2 /mnt/persist
 mount -o compress=zstd,noatime,subvol=nix /dev/sda2 /mnt/nix
 mount -o subvol=swap /dev/sda2 /mnt/swap
-nixos-install --flake /root/nixos-config#kerguelen --no-root-password --option store /mnt/nix/store --root /mnt
+nixos-install --flake /root/nixos-config#kerguelen --no-root-password --root /mnt
