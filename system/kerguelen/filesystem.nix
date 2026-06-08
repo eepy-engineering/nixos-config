c92cfa99e7c4028dab3310a318e550c21a5c02f5
Aubrey Taylor <aubrey@hall.ly>
2026-06-07 21:11:59 -0600

    add boot part to nixos config

--- system/kerguelen/filesystem.nix
+++ system/kerguelen/filesystem.nix
@@ -1,33 +1,43 @@
 {
-
-  fileSystems."/" = {
-    device = "tmpfs";
-    fsType = "tmpfs";
-  };
-
-  fileSystems."/persist" = {
-    device = "/dev/disk/by-uuid/15ea7064-494a-4d56-b4f8-6897054f7fa3";
-    fsType = "btrfs";
-    options = [
-      "compress=zstd"
-      "subvol=persist"
-    ];
-  };
-
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/15ea7064-494a-4d56-b4f8-6897054f7fa3";
-    fsType = "btrfs";
-    options = [
-      "compress=zstd"
-      "noatime"
-      "subvol=nix"
-    ];
-  };
-
-  fileSystems."/swap" = {
-    device = "/dev/disk/by-uuid/15ea7064-494a-4d56-b4f8-6897054f7fa3";
-    fsType = "btrfs";
-    options = [ "subvol=swap" ];
+  fileSystems = {
+    "/" = {
+      device = "tmpfs";
+      fsType = "tmpfs";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-partuuid/7594381a-8ef1-487b-b1ad-1ca83f128216";
+      fsType = "vfat";
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
+      depends = [ "/" ];
+    };
+    "/persist" = {
+      device = "/dev/disk/by-uuid/15ea7064-494a-4d56-b4f8-6897054f7fa3";
+      fsType = "btrfs";
+      options = [
+        "compress=zstd"
+        "subvol=persist"
+      ];
+      depends = [ "/" ];
+    };
+    "/nix" = {
+      device = "/dev/disk/by-uuid/15ea7064-494a-4d56-b4f8-6897054f7fa3";
+      fsType = "btrfs";
+      options = [
+        "compress=zstd"
+        "noatime"
+        "subvol=nix"
+      ];
+      depends = [ "/" ];
+    };
+    "/swap" = {
+      device = "/dev/disk/by-uuid/15ea7064-494a-4d56-b4f8-6897054f7fa3";
+      fsType = "btrfs";
+      options = [ "subvol=swap" ];
+      depends = [ "/" ];
+    };
   };
 
   swapDevices = [ { device = "/swap/swap.mem"; } ];
